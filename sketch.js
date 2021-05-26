fc865569431fa65312cafba02375ea5f214096f2
KYTO1234 <79738839+KYTO1234@users.noreply.github.com>
2021-05-26 18:12:46 +1200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+var bgImg;
+var cat , catImg1, catImg2, catImg3;
+var rat, ratImg1, ratImg2, ratImg3;
+
+function preload() {
+    //load the images here
+    bgImg = loadImage("garden.png");
+
+    catImg1= loadAnimation("images/cat1.png"); 
+    catImg2=loadAnimation("images/cat2.png","images/cat3.png"); 
+    catImg3= loadAnimation("images/cat4.png");
+    ratImg1=loadAnimation("images/jerryOne.png");
+    ratImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
+    ratImg3=loadAnimation("images/jerryFour.png");
+
+}
+
+function setup(){
+    createCanvas(400,400);
+    //create tom and jerry sprites here
+    cat = createSprite(870, 600);
+    cat.addAnimation("catSleeping", catImg1);
+    cat.scale = 0.2;
+
+    rat = createSprite(200, 600);
+    rat.addAnimation("ratStanding", ratImg1);
+    rat.scale = 0.18;
+
+}
+
+function draw() {
+
+    background(bg);
+    //Write condition here to evalute if tom and jerry collide
+    if(cat.x - rat.x < (cat.width - rat.width)/2)
+    { 
+        cat.velocityX=0;
+        cat.addAnimation("tomLastImage", catImg3);
+        cat.x =300;
+        cat.scale=0.2;
+        cat.changeAnimation("catLastImage");
+        rat.addAnimation("ratLastImage", ratImg3);
+        rat.scale=0.15;
+        rat.changeAnimation("ratLastImage");
+    }  
+   
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+  if(keycode === LEFT_ARROW){
+    cat.velocityX = -5; 
+    cat.addAnimation("catRunning", catImg2);
+    cat.changeAnimation("catRunning");
+
+  }
+
+}
